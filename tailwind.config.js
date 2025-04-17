12cb8f9bd703cad4a7051b8cd72c38b5f3f89e19
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-04-17 18:09:30 +0200

    feat: apply custom font

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                volkhov: ['Volkhov', 'serif'], 
+            },
+        },
+    },
+    plugins: [],
+}
